bef1ef670a32c3aea816e07c7bdb9cd0fc235ed4
BlackOSSoftware <blackossoftwaresolution@gmail.com>
2026-02-08 03:21:13 +0530

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,7 +7,13 @@ const config: CapacitorConfig = {
   server: {
     url: 'https://trade-pi-khaki.vercel.app',
     cleartext: false
-  }
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      autoHide: false,
+    },
+  },
 };
 
 export default config;
